88890bd96dea54c773e6234236e087b5a328a34c
Nithiish S.D <nithiishdanasekar@gmail.com>
2026-02-02 16:00:26 +0530

    duggingfeb_2

--- frontend/src/components/sections/ProblemDomains.tsx
+++ frontend/src/components/sections/ProblemDomains.tsx
@@ -173,12 +173,12 @@ export function ProblemDomains() {
                       stroke="currentColor"
                       viewBox="0 0 24 24"
                     >
-                      <path
+                      {/* <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
                         strokeWidth={2}
                         d="M9 5l7 7-7 7"
-                      />
+                      /> */}
                     </svg>
                   </div>
                 </div>
